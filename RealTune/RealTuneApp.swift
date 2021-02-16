d9e53ff067f28f435c9b793d74e0f60f4cc0697e
Josh Gerontis <gerontisj@yahoo.com>
2021-02-15 21:17:59 -0700

    Initial Commit

---
+++ RealTune/RealTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTuneApp.swift
+//  RealTune
+//
+//  Created by Josh Gerontis on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct RealTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
